367e3fff081269adfbee5c14571eb145499bd6b3
Quincy <qsyhuang@gmail.com>
2025-04-07 14:43:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double num1 = 0;
+double num2 = 0;
+
+Console.WriteLine("Add two numbers together, give me two numbers to add: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+num2 = Convert.ToDouble(Console.ReadLine());
+
+double sum = num1 + num2;
+Console.WriteLine("The sum of your two numbers is: " + sum);
+
